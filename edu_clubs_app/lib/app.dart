703dcff41bf432ba12668235fbfb8a26a7c6f461
Mishad <mishadcs2002@gmail.com>
2024-10-22 23:44:07 +0600

    Sign In Screen added and Animation added on Splash Screen

--- edu_clubs_app/lib/app.dart
+++ edu_clubs_app/lib/app.dart
@@ -1,14 +1,41 @@
-import 'package:edu_clubs_app/presentation/ui/screens/auth_screen/sign_in_screen.dart';
+import 'package:edu_clubs_app/presentation/ui/screens/auth_screen/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class EduClubs extends StatelessWidget {
   const EduClubs({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
-      home: SignInScreen(),
+    return GetMaterialApp(
+      theme: _themeData(),
+      initialRoute: '/',
+      getPages: [
+        GetPage(name: '/', page: () => SplashScreen()),
+      ],
+    );
+  }
+
+  ThemeData _themeData() {
+    return ThemeData(
+      textTheme: GoogleFonts.robotoTextTheme().copyWith(
+        bodySmall: GoogleFonts.roboto(color: Colors.black),
+        bodyLarge: GoogleFonts.roboto(color: Colors.black),
+        titleSmall: GoogleFonts.roboto(color: Colors.black),
+      ),
+      inputDecorationTheme: InputDecorationTheme(
+        hintStyle: GoogleFonts.roboto(color: Colors.grey, fontSize: 15),
+        focusedBorder: const UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.black, width: 1.5),
+        ),
+        enabledBorder: const UnderlineInputBorder(
+          borderSide: BorderSide(width: 0.9),
+        ),
+        errorBorder: const UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.red, width: 1.5),
+        ),
+      ),
     );
   }
 }
--- edu_clubs_app/lib/presentation/ui/screens/auth_screen/sign_in_screen.dart
+++ edu_clubs_app/lib/presentation/ui/screens/auth_screen/sign_in_screen.dart
@@ -1,5 +1,9 @@
+import 'package:edu_clubs_app/presentation/utility/assets_path.dart';
 import 'package:edu_clubs_app/presentation/widgets/background_widget.dart';
+import 'package:edu_clubs_app/presentation/widgets/positioned_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SignInScreen extends StatefulWidget {
   const SignInScreen({super.key});
@@ -9,10 +13,177 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
+  final TextEditingController _emailTEController = TextEditingController();
+  final TextEditingController _passwordTEController = TextEditingController();
+  final _formState = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
+    TextTheme textTheme = Theme.of(context).textTheme;
     return Scaffold(
-      body: BackgroundWidget(child: Center(child: Text("EDU CLUB"))),
+      body: BackgroundWidget(
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+              key: _formState,
+              child: Stack(
+                children: [
+                  Stack(
+                    children: [
+                      PositionedWidget(
+                        top: 1,
+                        child: Center(
+                          child: SvgPicture.asset(
+                            AssetsPath.eduLogo,
+                            height: 241,
+                            width: 288,
+                          ),
+                        ),
+                      ),
+                      PositionedWidget(
+                          top: 180,
+                          child: Container(
+                            //color: Colors.yellow,
+                            height: 140,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "WELCOME BACK",
+                                  style: GoogleFonts.roboto(fontSize: 38),
+                                ),
+                                Baseline(
+                                  baseline: 80,
+                                  baselineType: TextBaseline.alphabetic,
+                                  child: Text(
+                                    "!",
+                                    style: GoogleFonts.gruppo(
+                                      fontSize: 120,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          )),
+                      PositionedWidget(
+                        top: 280,
+                        child: Center(
+                          child: Text(
+                            "Please sign in to continue",
+                            style: GoogleFonts.roboto(fontSize: 15),
+                          ),
+                        ),
+                      ),
+                      PositionedWidget(
+                        top: 350,
+                        child: Container(
+                          width: 352,
+                          height: 180,
+                          decoration: BoxDecoration(
+                            color: const Color(0xffD0D9FC).withOpacity(0.20),
+                            borderRadius: const BorderRadius.all(
+                              Radius.circular(20),
+                            ),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: Column(
+                              children: [
+                                TextFormField(
+                                  controller: _emailTEController,
+                                  decoration:
+                                      InputDecoration(hintText: "Email"),
+                                  validator: (value) {
+                                    if (value == null || value.isEmpty) {
+                                      return "Please Input Correct Email";
+                                    }
+                                    String pattern =
+                                        r'^[a-zA-Z0-9._%+-]+@[eastdelta\.edu\.bd]$';
+                                    RegExp regex = RegExp(pattern);
+                                    if (!regex.hasMatch(value)) {
+                                      return 'Please enter a valid email address';
+                                    }
+                                    return null;
+                                  },
+                                ),
+                                SizedBox(height: 30),
+                                TextFormField(
+                                  controller: _passwordTEController,
+                                  decoration: const InputDecoration(
+                                      hintText: "Password"),
+                                  validator: (value) {
+                                    if (value == null || value.isEmpty) {
+                                      return "Please Input Correct Email";
+                                    }
+                                    if (value.length < 8) {
+                                      return 'Password must be at least 8 characters long';
+                                    }
+                                    return null;
+                                  },
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      PositionedWidget(
+                        top: 550,
+                        child: Center(
+                          // Center aligns the button within the available space
+                          child: Container(
+                            width: 108,
+                            height: 40,
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: const Color(0xffFDEBB9),
+                                  minimumSize: Size(108, 40)),
+                              child: const Text(
+                                "Login",
+                                style: TextStyle(
+                                  fontSize: 19,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      PositionedWidget(
+                        top: 600,
+                        child: Center(
+                          // Center aligns the button within the available space
+                          child: RichText(
+                            text: TextSpan(
+                              text: "Don't Have an account? ",
+                              style: textTheme.bodyLarge!.copyWith(
+                                  fontWeight: FontWeight.w400, fontSize: 17),
+                              children: const [
+                                TextSpan(
+                                  text: "Sign Up",
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _emailTEController.dispose();
+    _passwordTEController.dispose();
   }
 }
